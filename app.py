b524d6cecbacd79d31fca6a4f5b8d69e2769d300
MauricioNoriegaTec <mau.noriega10@gmail.com>
2024-09-14 15:13:16 -0600

    html, css, debug=true

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello orld'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
